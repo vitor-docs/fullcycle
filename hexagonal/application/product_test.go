074f5c3d0de090e1d5ed20af5764d065dafecc8a
Vitor <vitor.documents@gmail.com>
2021-03-28 22:40:05 -0300

    add hexagonal

---
+++ hexagonal/application/product_test.go
@@ -0,0 +1,17 @@
+package application_test
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hello"
+	product.Status = application.DISABLED
+	product.Price = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+}
